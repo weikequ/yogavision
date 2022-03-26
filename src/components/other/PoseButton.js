d6084324701a6e66aff62684b6e4c634ebf7d9c0
matth <mamatthe@students.cs.ubc.ca>
2022-03-26 14:47:55 -0700

    feat: updated pose buttons

--- src/components/other/PoseButton.js
+++ src/components/other/PoseButton.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import IconButton from "@mui/material/IconButton";
 
-const PoseButton = ({poseName, image}) => {
+const PoseButton = ({ poseName, image }) => {
   return (
     <div>
-      
+      <IconButton>
+        <img src={image}></img>
+      </IconButton>
+      <div>{poseName}</div>
     </div>
-  )
-}
+  );
+};
 
-export default PoseButton
+export default PoseButton;
