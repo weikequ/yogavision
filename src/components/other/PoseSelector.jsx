4e7edfe765f41c8c085a12f51bfd310e22426b49
matth <mamatthe@students.cs.ubc.ca>
2022-03-26 17:47:08 -0700

    feat: done

--- src/components/other/PoseSelector.jsx
+++ src/components/other/PoseSelector.jsx
@@ -2,22 +2,33 @@ import React from "react";
 import PoseButton from "./PoseButton";
 import treePose from "../../media/tree-pose.svg";
 import warriorPose from "../../media/warrior-pose.svg";
-import goodposeData from "../../data/goodPoses.json"
+import goodposeData from "../../data/goodPoses.json";
 
-const PoseSelector = ({ goodPose, score, switchGoodPose}) => {
-
-  const tree = goodposeData[0].data
-  const warrior = goodposeData[1].data
+const PoseSelector = ({ goodPose, score, switchGoodPose }) => {
+  const tree = goodposeData[0].data;
+  const warrior = goodposeData[1].data;
 
   return (
-    <div>
+    <div >
       <h1 style={{ padding: 20 }}>Your Score:</h1>
       <h3 className="text-center">{score.toFixed()}</h3>
       <h1 style={{ padding: 20 }}>Select Your Pose:</h1>
       {/* <h4>{goodPose}</h4> */}
       <div style={{ padding: 10 }} className="">
-        <PoseButton switchGoodPose={switchGoodPose} data={tree} poseName="Tree Pose" image={treePose} />
-        <PoseButton switchGoodPose={switchGoodPose} data={warrior} poseName="Warrior Pose" image={warriorPose} />
+        <PoseButton
+          style={{ padding: 80 }}
+          switchGoodPose={switchGoodPose}
+          data={tree}
+          poseName="Tree Pose"
+          image={treePose}
+        />
+        <PoseButton
+          style={{ padding: 80 }}
+          switchGoodPose={switchGoodPose}
+          data={warrior}
+          poseName="Warrior Pose"
+          image={warriorPose}
+        />
       </div>
     </div>
   );
--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -2,35 +2,40 @@ import React, { useState, useEffect } from "react";
 import RouterButton from "../other/RouterButton";
 import VideoFeed from "../other/VideoFeed";
 import PoseSelector from "../other/PoseSelector";
-import {findAverageScore, findScores} from "../../logic/scoreCalculation";
-import goodposeData from "../../data/goodPoses.json"
-
+import { findAverageScore, findScores } from "../../logic/scoreCalculation";
+import goodposeData from "../../data/goodPoses.json";
+import bgSVG from "../../media/background.svg";
 
 const Main = () => {
-
-  const tree = goodposeData[0].data
-  const warrior = goodposeData[1].data
+  const tree = goodposeData[0].data;
+  const warrior = goodposeData[1].data;
 
   const [poseState, setPoseState] = useState(null);
-  const [goodPose, setGoodPose] = useState(null)
+  const [goodPose, setGoodPose] = useState(null);
   const [score, setScore] = useState(0);
 
   useEffect(() => {
-   (poseState !== null && goodPose !== null) && setScore(findAverageScore(poseState, goodPose));
-  }, [poseState])
-  
+    poseState !== null &&
+      goodPose !== null &&
+      setScore(findAverageScore(poseState, goodPose));
+  }, [poseState]);
+
   const switchGoodPose = (pose) => {
-    setGoodPose(pose)
-  }
+    setGoodPose(pose);
+  };
 
   return (
-    <>
-    <RouterButton link="/" text="About" variant="contained" />
-    <div style={{display:'flex'}}>
-      <PoseSelector goodPose={goodPose} score={score} switchGoodPose={switchGoodPose}/>
-      <VideoFeed setPoseState={setPoseState} />
+    <div style={{ backgroundImage: `url(${bgSVG})` }}>
+      <RouterButton link="/" text="About" variant="contained" />
+      <div style={{ display: "flex", height: 2000 }}>
+        <PoseSelector
+          goodPose={goodPose}
+          score={score}
+          switchGoodPose={switchGoodPose}
+        />
+        <VideoFeed setPoseState={setPoseState} />
+      </div>
     </div>
-    </>
   );
 };
 
