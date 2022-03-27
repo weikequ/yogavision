3039b615bf4f1e7040323dce3822c355fea5f3fe
Weike Qu <weikequ@hotmail.com>
2022-03-26 17:49:04 -0700

    fix: last change

--- src/components/other/RouterButton.js
+++ src/components/other/RouterButton.js
@@ -3,8 +3,10 @@ import Button from "@mui/material/Button";
 
 const RouterButton = ({ link, text, variant }) => {
   return (
-    <div style={{ position: "absolute", left: 0, bottom: 0 }}>
-      <Button href={link} variant={variant}>
+    <div style={{ position: "absolute", left: 30, bottom: 10 }}>
+      <Button style={{
+        backgroundColor: "#433030"
+        }} href={link} variant={variant}>
         {text}
       </Button>
     </div>
